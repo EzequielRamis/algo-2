c55c173c589b7f0c12c722ec19607c59cc8a6ca8
Diego <diego98g@hotmail.com>
2022-11-11 21:38:44 -0300

    T02

--- T02/src/Variante.cpp
+++ T02/src/Variante.cpp
@@ -1,12 +1,27 @@
 #include "Variante.h"
-#include "Letra.h"
 
 Variante::Variante(
         Nat tamanoTab,
         Nat cantFichas,
         const map<Letra, Nat> &puntajes,
         const set<vector<Letra>> &palabrasLegitimas
-) : _puntaje(vector<Nat>()), _fichas(cantFichas), _tablero(tamanoTab) {}
+) : _tablero(tamanoTab),
+    _fichas(cantFichas),
+    _puntaje(new Nat[TAMANIO_ALFABETO]),
+        /*_palabra(Trie constructor),*/
+    _palabraMasLarga(0) {
+    for (int i = 0; i < TAMANIO_ALFABETO; i++) {
+        if (puntajes.count(inversaDeOrd(i)) > 0)
+            _puntaje[i] = puntajes.at(inversaDeOrd(i));
+        else
+            _puntaje[i] = 1;
+    }
+    /*for (vector<Letra> pal : palabrasLegitimas){
+        _palabra.insert(pal);
+        if (pal.size() > _palabraMasLarga)
+            _palabraMasLarga = pal.size();
+    }*/
+}
 
 Nat Variante::tamanoTablero() const {
     return _tablero;
@@ -21,6 +36,6 @@ Nat Variante::puntajeLetra(Letra l) const {
 }
 
 bool Variante::palabraLegitima(const Palabra &palabra) const {
-    //return palabras.count(palabra) == 1;
+    //return _palabra.count(palabra) == 1;
     return true;
 }
